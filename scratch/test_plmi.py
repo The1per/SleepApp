d28763cb875dbaf750dd057666234971cc97d933
ynir mfa <ynirmfa@tau.ac.il>
2026-06-23 14:47:03 +0300

    Clean up repository: remove generated files, test scripts, and reports

--- scratch/test_plmi.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('c:/Users/ynirmfa/Desktop/app')
-from bidi.algorithm import get_display
-
-with open("c:/Users/ynirmfa/Desktop/app/scratch/test_out.txt", "w", encoding="utf-8") as f:
-    f.write(get_display("4.1 / שעה"))
